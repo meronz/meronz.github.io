9aedc0d2ffb3c5cb452bfec26fd20fc3e3d1d449
Salvatore Merone <sal.merone95@gmail.com>
2024-11-10 19:32:23 +0100

    cleanup pages and added useful, real links

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -41,18 +41,14 @@ const siteConfig: SiteConfig = {
     title: 'meronz',
     //subtitle: 'Minimal Astro.js theme',
     description: 'Home page of Salvatore Merone (meronz), an Italian software developer.',
-    image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
-    },
+    // image: {
+    //     src: '/dante-preview.jpg',
+    //     alt: 'Dante - Astro.js and Tailwind CSS theme'
+    // },
     headerNavLinks: [
         {
             text: 'Home',
             href: '/'
-        },
-        {
-            text: 'Projects',
-            href: '/projects'
         },
         {
             text: 'Blog',
@@ -78,32 +74,36 @@ const siteConfig: SiteConfig = {
         // }
     ],
     socialLinks: [
-        // {
-        //     text: 'Dribbble',
-        //     href: 'https://dribbble.com/'
-        // },
-        // {
-        //     text: 'Instagram',
-        //     href: 'https://instagram.com/'
-        // },
-        // {
-        //     text: 'X/Twitter',
-        //     href: 'https://twitter.com/'
-        // }
+        {
+            text: 'Linkedin',
+            href: 'https://linkedin.com/in/meronz'
+        },
+        {
+            text: 'Github',
+            href: 'https://github.com/meronz'
+        },
+        {
+            text: 'Sessionize',
+            href: 'https://sessionize.com/meronz'
+        },
+        {
+            text: '@meronz@hachyderm.io',
+            href: 'https://hachyderm.io/@meronz'
+        },
     ],
     hero: {
-        title: 'You reached /home/meronz.',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
+        title: 'Hello! 👋🏻',
+        //text: "",
+        // image: {
+        //     src: '/hero.jpeg',
+        //     alt: 'A person sitting at a desk in front of a computer'
+        // },
+        // actions: [
+        //     {
+        //         text: 'Contacts',
+        //         href: '/contact'
+        //     }
+        // ]
     },
     subscribe: {
         title: 'Subscribe to Dante Newsletter',
